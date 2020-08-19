c273789179fd9d2eda0d401291479c5902a8aba0
gabgarble <gabgarble@gmail.com>
2020-08-19 11:44:26 +0200

    Creación de nuevos componentes y estructura básica de código

---
+++ src/components/Settings.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<!-- Script -->
+<script>
+
+export default {
+  name: "create-event-form",
+  data: () => ({
+    
+  }),
+  validations: {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
